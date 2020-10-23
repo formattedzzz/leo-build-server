6ed7254ebcc04aaccee1d09b8963e186d8b20cac
formattedzzz <614791110@qq.com>
2020-10-23 18:12:47 +0800

    refine

--- router/resumption.js
+++ router/resumption.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer')
 // const path = require('path')
 
 router.get('/', async function (req, res) {
-  const { url } = req.query
+  const { url, sec } = req.query
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
   const page = await browser.newPage()
   await page
@@ -12,16 +12,19 @@ router.get('/', async function (req, res) {
       waitUntil: 'domcontentloaded'
     })
     .then(() => {
-      setTimeout(() => {
-        page.close()
-        res.json({
-          code: 200,
-          data: {
-            url
-          },
-          message: '访问成功'
-        })
-      }, 6000)
+      setTimeout(
+        () => {
+          page.close()
+          res.json({
+            code: 200,
+            data: {
+              url
+            },
+            message: '访问成功'
+          })
+        },
+        sec ? sec * 1000 : 6000
+      )
     })
     .catch(err => {
       console.log(err)
