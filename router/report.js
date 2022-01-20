76bc27dd9805f4217e0f5d44f38833e15e0b93e4
formattedzzz <614791110@qq.com>
2022-01-20 16:45:47 +0800

    feat: shot

--- router/report.js
+++ router/report.js
@@ -1,52 +1,65 @@
 let express = require('express')
 let router = express.Router()
-const puppeteer = require('puppeteer');
-const path = require('path');
+const puppeteer = require('puppeteer')
+const path = require('path')
 
-router.get('/', async function(req, res){
-  let {skuid, sessionid} = req.query;
-  if (!skuid || !sessionid) {
+router.get('/', async function (req, res) {
+  let { url, width = 750, height = 1334 } = req.query
+  if (!url) {
     res.json({
       code: 0,
       data: null,
       message: '截屏失败'
     })
+    return
   }
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+  const page = await browser.newPage()
   await page.setViewport({
-    width: 750,
-    height: 1334
-  });
-  let request_url = `https://ecs.nncz.cn/report/summary/${skuid}/${sessionid}#ping`;
-  await page.goto(request_url, {
-    waitUntil: 'domcontentloaded'
-  }).catch(err => {
-    console.log(err)
-    res.json({
-      code: 0,
-      data: null,
-      message: '截屏失败'
+    width,
+    height
+  })
+  await page
+    .goto(url, {
+      waitUntil: 'domcontentloaded'
     })
-  });
-  try {
-    console.log(path.join(__dirname, `../static/img/${skuid + sessionid}.jpg`))
-    await page.screenshot({
-      path: path.join(__dirname, `../static/img/${skuid + sessionid}.jpg`),
-      fullPage: true,
-      // type: 'png',
-      encoding: 'gbk' 
-    }).catch(err => {
+    .catch(err => {
+      console.log(err)
       res.json({
         code: 0,
         data: null,
         message: '截屏失败'
       })
-    });
-    res.json({
-      code: 1,
-      data: `https://wx.nnleo.cn/static/img/${skuid + sessionid}.jpg`,
-      message: '截屏成功'
+    })
+  try {
+    const timestamp = Date.now()
+    const temppath = path.join(__dirname, `../static/img/${timestamp}.jpg`)
+    await page
+      .screenshot({
+        path: temppath,
+        fullPage: true,
+        // type: 'png',
+        encoding: 'gbk'
+      })
+      .catch(err => {
+        res.json({
+          code: 0,
+          data: null,
+          message: '截屏失败'
+        })
+      })
+    res.set('Content-Type', 'image/jpg; charset=utf-8')
+    let url = path.resolve(temppath)
+    fs.readFile(url, function (err, data) {
+      if (err) {
+        res.header('Content-Type', 'application/json;charset=utf-8')
+        res.status(404).json({
+          code: 0,
+          message: '404!没有找到相应资源，请检查路径'
+        })
+      } else {
+        res.send(data)
+      }
     })
   } catch (e) {
     res.json({
@@ -55,7 +68,7 @@ router.get('/', async function(req, res){
       message: '截屏失败'
     })
   } finally {
-    await browser.close();
+    await browser.close()
   }
 })
 module.exports = router
